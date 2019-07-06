5ad118c361705615a12f57548117b23d676d4c71
grigor-aramyan <grigori.aramyan@gmail.com>
2019-07-06 10:21:18 +0400

    add course update endpoint

--- src/server/routes/courseRoutes.js
+++ src/server/routes/courseRoutes.js
@@ -4,8 +4,72 @@ import auth from '../../../middleware/auth';
 
 // Schemas
 import Course from '../schemas/CourseSchema';
+import Teacher from '../schemas/TeacherSchema';
 
 const router = express.Router();
+
+// @route PUT api/courses/:id
+// @desc Update course data
+// @access Private
+router.put('/:id', auth, function(req, res) {
+    const teacherId = mongoose.Types.ObjectId(req.user.id);
+    const dataObject = req.body;
+
+    let courseId = null;
+    try {
+        courseId = mongoose.Types.ObjectId(req.params.id);
+    } catch(e) {
+        return res.status(400).json({ msg: 'Bad request!' });
+    }
+    
+    try {
+        if (dataObject.lessons) {
+            if (Object.prototype.toString.call(dataObject.lessons) === '[object Array]') {
+                if (dataObject.lessons.length == 0) {
+                    return res.status(400).json({ msg: 'Course can\'t contain 0 lessons' });
+                }
+            } else {
+                return res.status(400).json({ msg: 'Bad request' });
+            }
+        }
+    } catch(e) {
+        return res.status(400).json({ msg: 'Bad request' });
+    }
+
+    Teacher.findById(teacherId, (err, teacher) => {
+        if (err || (teacher == null)) return res.status(400).json({ msg: 'No teacher found with provided credentials' });
+
+        Course.findOne({ _id: courseId, author: teacher._id }, (err, course) => {
+            if (err || (course == null)) return res.status(400).json({ msg: 'No course found with provided credentials' });
+
+            if (dataObject.lessons) {
+                const lessonsObjectIds = dataObject.lessons.filter(l => {
+                    try {
+                        mongoose.Types.ObjectId(l);
+                        return true;
+                    } catch(e) {
+                        return false;
+                    }
+                }).map(l => { return mongoose.Types.ObjectId(l); });
+
+                if (lessonsObjectIds.length == 0) return res.status(400).json({ msg: 'Course can\t contain 0 lessons' });
+
+                dataObject.lessons = lessonsObjectIds;
+                Course.updateOne({ _id: course._id }, dataObject, (err, affected, resp) => {
+                    if (err) return res.status(500).json({ msg: 'Internal error. Try later, please!' });
+    
+                    res.status(200).json({ msg: 'success' });
+                });
+            } else {
+                Course.updateOne({ _id: course._id }, dataObject, (err, affected, resp) => {
+                    if (err) return res.status(500).json({ msg: 'Internal error. Try later, please!' });
+    
+                    res.status(200).json({ msg: 'success' });
+                });
+            }
+        });
+    });
+});
 
 // @route POST api/courses
 // @desc Add new course
