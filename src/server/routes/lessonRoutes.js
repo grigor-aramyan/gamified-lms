a093c3355b29086490283e05a1b9422c5023e7e4
grigor-aramyan <grigori.aramyan@gmail.com>
2019-07-06 05:02:16 +0400

    add lesson update endpoint

--- src/server/routes/lessonRoutes.js
+++ src/server/routes/lessonRoutes.js
@@ -8,6 +8,35 @@ import Teacher from '../schemas/TeacherSchema';
 
 const router = express.Router();
 
+// @route PUT api/lessons/:id
+// @desc Update lesson data
+// @access Private
+router.put('/:id', auth, function(req, res) {
+    const dataObject = req.body;
+    const teacherId = mongoose.Types.ObjectId(req.user.id);
+    let lessonId = null;
+
+    try {
+        lessonId = mongoose.Types.ObjectId(req.params.id);
+    } catch(e) {
+        return res.status(400).json({ msg: 'Bad request!' });
+    }
+
+    Teacher.findById(teacherId, (err, teacher) => {
+        if (err || (teacher == null)) return res.status(400).json({ msg: 'No teacher found with provided credentials' });
+
+        Lesson.findOne({ _id: lessonId, author: teacherId }, (err, lesson) => {
+            if (err || (lesson == null)) return res.status(400).json({ msg: 'No lesson found with provided credentials' });
+            
+            Lesson.updateOne({ _id: lesson._id }, dataObject, (err, affected, resp) => {
+                if (err) return res.status(500).json({ msg: 'Internal error. Try later, please' });
+
+                res.status(200).json({ msg: 'success' });
+            });
+        });
+    });
+});
+
 // @route POST api/lessons
 // @desc Add new lesson
 // @access Private
