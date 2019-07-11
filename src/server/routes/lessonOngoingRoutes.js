9c46c32dd825d35015f97c4b1ce14706b94e49c2
grigor-aramyan <grigori.aramyan@gmail.com>
2019-07-11 23:18:33 +0400

    add create lesson ongoing for logged in learner endpoint

--- src/server/routes/lessonOngoingRoutes.js
+++ src/server/routes/lessonOngoingRoutes.js
@@ -5,8 +5,48 @@ import auth from '../../../middleware/auth';
 // Schemas
 import LessonOngoing from '../schemas/LessonOngoingSchema';
 import Learner from '../schemas/LearnerSchema';
+import Lesson from '../schemas/LessonSchema';
 
 const router = express.Router();
+
+// @route POST api/lesson_ongoings
+// @desc Create lesson ongoing for logged in learner
+// @access Private
+router.post('/', auth, function(req, res) {
+    const learnerId = mongoose.Types.ObjectId(req.user.id);
+
+    let lessonId = null;
+    try {
+        lessonId = mongoose.Types.ObjectId(req.body.lessonId);
+    } catch(e) {
+        return res.status(400).json({ msg: 'No lesson found with provided credentials' });
+    }
+
+    Learner.findById(learnerId, (err, learner) => {
+        if (err || (learner == null)) return res.status(400).json({ msg: 'No learner found with provided credentials' });
+
+        Lesson.findById(lessonId, (err, lesson) => {
+            if (err || (lesson == null)) return res.status(400).json({ msg: 'No lesson found with provided credentials' });
+
+            const newLessonOngoing = new LessonOngoing({
+                learnerId: learner._id,
+                lessonId: lesson._id
+            });
+
+            newLessonOngoing.save()
+                .then(lo => {
+                    res.status(201).json({
+                        lesson_ongoing: {
+                            id: lo._id,
+                            completed: lo.completed,
+                            lessonId: lo.lessonId,
+                            learnerId: lo.learnerId 
+                        }
+                    });
+                });
+        });
+    });
+});
 
 // @route GET api/lesson_ongoings
 // @desc Get lesson ongoings of logged in learner
