d5ca2e7812df58ba244fd67f3369ca9d6c4504ff
grigor-aramyan <grigori.aramyan@gmail.com>
2019-07-11 21:20:29 +0400

    add get lesson ongoings of logged in learner endpoint

--- src/server/routes/lessonOngoingRoutes.js
+++ src/server/routes/lessonOngoingRoutes.js
@@ -4,14 +4,34 @@ import auth from '../../../middleware/auth';
 
 // Schemas
 import LessonOngoing from '../schemas/LessonOngoingSchema';
+import Learner from '../schemas/LearnerSchema';
 
 const router = express.Router();
 
 // @route GET api/lesson_ongoings
-// @desc Get lesson ongoings
+// @desc Get lesson ongoings of logged in learner
 // @access Private
 router.get('/', auth, function(req, res) {
-    res.json({ msg: 'done lesson ongoings' });
+    const learnerId = mongoose.Types.ObjectId(req.user.id);
+
+    Learner.findById(learnerId, (err, learner) => {
+        if (err || (learner == null)) return res.status(400).json({ msg: 'No learner found with provided credentials' });
+
+        LessonOngoing.find({ learnerId: learner._id }, (err, lessonOngoings) => {
+            if (err) return res.status(400).json({ msg: 'Bad request!' });
+
+            const lessonOngoingsFiltered = lessonOngoings.map(l => {
+                return {
+                    id: l._id,
+                    completed: l.completed,
+                    lessonId: l.lessonId,
+                    completionPoint: l.completionPoint
+                };
+            });
+
+            res.status(200).json({ lesson_ongoings: lessonOngoingsFiltered });
+        });
+    });
 });
 
 export default router;
