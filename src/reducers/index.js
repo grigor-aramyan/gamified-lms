038c2130c4bf2462eaa8a2ba4fbafceb805c9a37
grigor-aramyan <grigori.aramyan@gmail.com>
2019-07-14 12:22:48 +0400

    add initial structure of reducers of redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,20 @@ import { combineReducers } from 'redux';
 
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
+import lessonReducer from './lessonReducer';
+import courseReducer from './courseReducer';
+import lessonOngoingReducer from './lessonOngoingReducer';
+import courseOngoingReducer from './courseOngoingReducer';
+import learnerReducer from './learnerReducer';
+import teacherReducer from './teacherReducer';
 
 export default combineReducers({
     auth: authReducer,
-    error: errorReducer
+    error: errorReducer,
+    lesson: lessonReducer,
+    course: courseReducer,
+    lessonOngoing: lessonOngoingReducer,
+    courseOngoing: courseOngoingReducer,
+    learner: learnerReducer,
+    teacher: teacherReducer
 });
