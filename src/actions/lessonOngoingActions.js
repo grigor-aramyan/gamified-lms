fe66e630c021a3d10e8dbc12729f11cefef0cb49
grigor-aramyan <grigori.aramyan@gmail.com>
2019-07-16 08:56:02 +0400

    add initial lesson ongoing reducer, actions and types of redux

---
+++ src/actions/lessonOngoingActions.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+
+import {
+    LESSON_ONGOING_CREATED_SUCCESS,
+    LESSON_ONGOING_CREATED_FAIL,
+    LESSON_ONGOINGS_GET_SUCCESS,
+    LESSON_ONGOINGS_GET_FAIL,
+    LESSON_ONGOING_UPDATE_SUCCESS,
+    LESSON_ONGOING_UPDATE_FAIL,
+    LESSON_ONGOING_DELETE_SUCCESS,
+    LESSON_ONGOING_DELETE_FAIL
+} from './types';
+import { returnErrors } from './errorActions';
+import { tokenConfig } from './authActions';
+
+// Constants
+const API_URI = 'http://localhost:4242/api/lesson_ongoings';
+
+export const updateLessonOngoing = (lessonOngoingId, newData) => (dispatch, getState) => {
+
+    const uri = `${API_URI}/${lessonOngoingId}`;
+
+    axios.put(uri, newData, tokenConfig(getState))
+        .then(res => {
+            // TODO if success, send 'newData' as payload to apply to learner in reducer
+            dispatch({
+                type: LESSON_ONGOING_UPDATE_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: LESSON_ONGOING_UPDATE_FAIL });
+        });
+}
+
+export const deleteLessonOngoing = (lessonOngoingId) => (dispatch, getState) => {
+
+    const uri = `${API_URI}/${lessonOngoingId}`;
+
+    axios.delete(uri, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: LESSON_ONGOING_DELETE_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: LESSON_ONGOING_DELETE_FAIL });
+        });
+}
+
+export const createLessonOngoing = (dataBody) => (dispatch, getState) => {
+    
+    axios.post(API_URI, dataBody, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: LESSON_ONGOING_CREATED_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: LESSON_ONGOING_CREATED_FAIL });
+        });
+}
+
+export const getLessonOngoings = () => (dispatch, getState) => {
+    
+    axios.get(API_URI, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: LESSON_ONGOINGS_GET_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: LESSON_ONGOINGS_GET_FAIL });
+        });
+}
--- src/reducers/lessonOngoingReducer.js
+++ src/reducers/lessonOngoingReducer.js
@@ -1,7 +1,53 @@
-const initialState = {};
+import {
+    LESSON_ONGOING_CREATED_SUCCESS,
+    LESSON_ONGOING_CREATED_FAIL,
+    LESSON_ONGOINGS_GET_SUCCESS,
+    LESSON_ONGOINGS_GET_FAIL,
+    LESSON_ONGOING_UPDATE_SUCCESS,
+    LESSON_ONGOING_UPDATE_FAIL,
+    LESSON_ONGOING_DELETE_SUCCESS,
+    LESSON_ONGOING_DELETE_FAIL
+} from './types';
+
+const initialState = {
+    currentLessonOngoing: null,
+    allLessonOngoings: []
+};
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        case LESSON_ONGOING_UPDATE_SUCCESS:
+            return {
+                ...state
+            };
+        case LESSON_ONGOING_DELETE_SUCCESS:
+            const deletedLessonOngoingId = action.payload.id;
+            const filteredLessonOngoings = state.allLessonOngoings.filter(l => {
+                if (l.id.toString() !== deletedLessonOngoingId.toString()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+            return {
+                ...state,
+                allLessonOngoings: filteredLessonOngoings
+            };
+        case LESSON_ONGOINGS_GET_SUCCESS:
+            return {
+                ...state,
+                allLessonOngoings: action.payload.lesson_ongoings
+            };
+        case LESSON_ONGOING_UPDATE_FAIL:
+        case LESSON_ONGOING_DELETE_FAIL:
+        case LESSON_ONGOINGS_GET_FAIL:
+        case LESSON_ONGOING_CREATED_FAIL:
+            return state;
+        case LESSON_ONGOING_CREATED_SUCCESS:
+            return {
+                ...state,
+                allLessonOngoings: state.allLessonOngoings.unshift(action.payload.lesson_ongoing)
+            };
         default:
             return state;
     }
