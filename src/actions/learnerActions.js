a1c30e01e04a0d1a16e1e1768f96c5ab2d409e17
grigor-aramyan <grigori.aramyan@gmail.com>
2019-07-16 05:31:17 +0400

    add initial learner reducer, actions and types of redux

---
+++ src/actions/learnerActions.js
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import { returnErrors } from './errorActions';
+import {
+    LEARNER_CREATED_SUCCESS,
+    LEARNER_CREATED_FAIL,
+    LEARNERS_GET_SUCCESS,
+    LEARNERS_GET_FAIL,
+    LEARNER_UPDATE_SUCCESS,
+    LEARNER_UPDATE_FAIL,
+    LEARNER_DELETE_SUCCESS,
+    LEARNER_DELETE_FAIL
+} from './types';
+import { tokenConfig } from './authActions';
+
+// Constants
+const API_URI = 'http://localhost:4242/api/learners';
+
+export const updateLearner = (newData) => (dispatch, getState) => {
+
+    axios.put(API_URI, newData, tokenConfig(getState))
+        .then(res => {
+            // TODO if success, send 'newData' as payload to apply to learner in reducer
+            dispatch({
+                type: LEARNER_UPDATE_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: LEARNER_UPDATE_FAIL });
+        });
+}
+
+export const deleteLearner = () => (dispatch, getState) => {
+
+    axios.delete(API_URI, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: LEARNER_DELETE_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: LEARNER_DELETE_FAIL });
+        });
+}
+
+export const createLearner = ({ name, email, password}) => (dispatch, getState) => {
+    const body = { name, email, password };
+
+    axios.post(API_URI, body, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: LEARNER_CREATED_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: LEARNER_CREATED_FAIL });
+        });
+}
+
+export const getLearners = () => (dispatch, getState) => {
+    
+    axios.get(API_URI, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: LEARNERS_GET_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: LEARNERS_GET_FAIL });
+        });
+}
--- src/reducers/learnerReducer.js
+++ src/reducers/learnerReducer.js
@@ -1,7 +1,51 @@
-const initialState = {};
+import {
+    LEARNER_CREATED_SUCCESS,
+    LEARNER_CREATED_FAIL,
+    LEARNER_UPDATE_SUCCESS,
+    LEARNER_UPDATE_FAIL,
+    LEARNER_DELETE_SUCCESS,
+    LEARNER_DELETE_FAIL,
+    LEARNERS_GET_SUCCESS,
+    LEARNERS_GET_FAIL
+} from '../actions/types';
+
+const initialState = {
+    token: '',
+    learner: null,
+    allLearners: []
+};
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        case LEARNER_UPDATE_SUCCESS:
+            return {
+                ...state
+            };
+        case LEARNER_UPDATE_FAIL:
+            return state;
+        case LEARNER_DELETE_SUCCESS:
+            return {
+                ...state,
+                token: '',
+                learner: null,
+                allLearners: []
+            };
+        case LEARNER_DELETE_FAIL:
+            return state;
+        case LEARNERS_GET_SUCCESS:
+            return {
+                ...state,
+                allLearners: action.payload
+            };
+        case LEARNERS_GET_FAIL:
+            return state;
+        case LEARNER_CREATED_FAIL:
+            return state;
+        case LEARNER_CREATED_SUCCESS:
+            return {
+                ...state,
+                ...action.payload
+            };
         default:
             return state;
     }
