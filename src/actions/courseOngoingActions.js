d21433951da7a0501355af097ddcab97bbc20e36
grigor-aramyan <grigori.aramyan@gmail.com>
2019-07-16 09:17:33 +0400

    add initial course ongoing reducer, actions and types of redux

---
+++ src/actions/courseOngoingActions.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+
+import {
+    COURSE_ONGOING_CREATED_SUCCESS,
+    COURSE_ONGOING_CREATED_FAIL,
+    COURSE_ONGOINGS_GET_SUCCESS,
+    COURSE_ONGOINGS_GET_FAIL,
+    COURSE_ONGOING_UPDATE_SUCCESS,
+    COURSE_ONGOING_UPDATE_FAIL,
+    COURSE_ONGOING_DELETE_SUCCESS,
+    COURSE_ONGOING_DELETE_FAIL
+} from './types';
+import { returnErrors } from './errorActions';
+import { tokenConfig } from './authActions';
+
+// Constants
+const API_URI = 'http://localhost:4242/api/course_ongoings';
+
+export const updateCourseOngoing = (courseOngoingId, newData) => (dispatch, getState) => {
+
+    const uri = `${API_URI}/${courseOngoingId}`;
+
+    axios.put(uri, newData, tokenConfig(getState))
+        .then(res => {
+            // TODO if success, send 'newData' as payload to apply to current course ongoing in reducer
+            dispatch({
+                type: COURSE_ONGOING_UPDATE_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: COURSE_ONGOING_UPDATE_FAIL });
+        });
+}
+
+export const deleteCourseOngoing = (courseOngoingId) => (dispatch, getState) => {
+
+    const uri = `${API_URI}/${courseOngoingId}`;
+
+    axios.delete(uri, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: COURSE_ONGOING_DELETE_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: COURSE_ONGOING_DELETE_FAIL });
+        });
+}
+
+export const createCourseOngoing = (dataBody) => (dispatch, getState) => {
+    
+    axios.post(API_URI, dataBody, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: COURSE_ONGOING_CREATED_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: COURSE_ONGOING_CREATED_FAIL });
+        });
+}
+
+export const getCourseOngoings = () => (dispatch, getState) => {
+    
+    axios.get(API_URI, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: COURSE_ONGOINGS_GET_SUCCESS,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+            dispatch({ type: COURSE_ONGOINGS_GET_FAIL });
+        });
+}
--- src/reducers/courseOngoingReducer.js
+++ src/reducers/courseOngoingReducer.js
@@ -1,7 +1,53 @@
-const initialState = {};
+import {
+    COURSE_ONGOING_CREATED_SUCCESS,
+    COURSE_ONGOING_CREATED_FAIL,
+    COURSE_ONGOINGS_GET_SUCCESS,
+    COURSE_ONGOINGS_GET_FAIL,
+    COURSE_ONGOING_UPDATE_SUCCESS,
+    COURSE_ONGOING_UPDATE_FAIL,
+    COURSE_ONGOING_DELETE_SUCCESS,
+    COURSE_ONGOING_DELETE_FAIL
+} from './types';
+
+const initialState = {
+    currentCourseOngoing: null,
+    allCourseOngoings: []
+};
 
 export default function(state = initialState, action) {
     switch(action.type) {
+        case COURSE_ONGOING_UPDATE_SUCCESS:
+            return {
+                ...state
+            };
+        case COURSE_ONGOING_DELETE_SUCCESS:
+            const deletedCourseOngoingId = action.payload.id;
+            const filteredCourseOngoings = state.allCourseOngoings.filter(l => {
+                if (l.id.toString() !== deletedCourseOngoingId.toString()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+            return {
+                ...state,
+                allCourseOngoings: filteredCourseOngoings
+            };
+        case COURSE_ONGOINGS_GET_SUCCESS:
+            return {
+                ...state,
+                allCourseOngoings: action.payload.course_ongoings
+            };
+        case COURSE_ONGOING_UPDATE_FAIL:
+        case COURSE_ONGOING_DELETE_FAIL:
+        case COURSE_ONGOINGS_GET_FAIL:
+        case COURSE_ONGOING_CREATED_FAIL:
+            return state;
+        case COURSE_ONGOING_CREATED_SUCCESS:
+            return {
+                ...state,
+                allCourseOngoings: state.allCourseOngoings.unshift(action.payload.course_ongoing)
+            };
         default:
             return state;
     }
