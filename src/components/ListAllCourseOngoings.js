c4fb0fe8be3e14811509b9f0fb86a54b2dcb6465
grigor-aramyan <grigori.aramyan@gmail.com>
2019-08-20 00:36:41 +0400

    get and show extended courses data

--- src/components/ListAllCourseOngoings.js
+++ src/components/ListAllCourseOngoings.js
@@ -14,16 +14,32 @@ import {
     getCourseOngoings
 } from '../actions/courseOngoingActions';
 
+import {
+    getExtendedCoursessById
+} from '../actions/courseActions';
+
 class ListAllCourseOngoings extends Component {
 
     componentDidMount() {
         this.props.getCourseOngoings();
     }
 
+    componentDidUpdate() {
+        if ((this.props.allCourseOngoings.length > 0)
+            && !this.props.coursesExtended
+            && !this.props.fetchingExtendedCourses) {
+            const coursesIds = this.props.allCourseOngoings.map(co => {
+                return co.courseId;
+            });
+            this.props.getExtendedCoursessById({ coursesIds });
+        }
+    }
+
     render() {
         const {
             deleteCourseOngoing,
-            allCourseOngoings
+            allCourseOngoings,
+            coursesExtended
         } = this.props;
 
         return(
@@ -31,25 +47,50 @@ class ListAllCourseOngoings extends Component {
                 <h2>My ongoing courses</h2>
                 <Row style={{ textAlign: 'center' }}>
                     <Col xs='4'>
-                        ID
+                        TITLE
                     </Col>
                     <Col xs='4'>
-                        COURSE ID
+                        LESSONS
+                    </Col>
+                    <Col xs='1'>
+                        PRICE
                     </Col>
                     <Col xs='2'>
                         DELETE
                     </Col>
                 </Row>
-                <ul>
+                <hr />
+                <ul style={{ listStyle: 'none', marginLeft: '0px' }}>
                     { allCourseOngoings.map(c => {
+                        let courseExtended = null;
+                        if (coursesExtended) {
+                            courseExtended = coursesExtended.filter(ce => {
+                                return(ce.id.toString() === c.courseId.toString());
+                            })[0];
+                        }
+
                         return(
                             <li key={c.id}>
                                 <Row>
                                     <Col xs='4'>
-                                        { c.id }
+                                        <a href='#'>{ courseExtended ? courseExtended.title : '' }</a>
                                     </Col>
                                     <Col xs='4'>
-                                        { c.courseId }
+                                        <ol>
+                                            { courseExtended ?
+                                                courseExtended.lessons.map(l => {
+                                                    return(
+                                                        <li key={l.id}>
+                                                            {l.title}
+                                                        </li>
+                                                    );
+                                                })
+                                            : null
+                                            }
+                                        </ol>
+                                    </Col>
+                                    <Col xs='1'>
+                                        { courseExtended ? `$${courseExtended.price}` : '' }
                                     </Col>
                                     <Col xs='2'>
                                         <Button 
@@ -76,15 +117,21 @@ ListAllCourseOngoings.propTypes = {
     error: PropTypes.object.isRequired,
     deleteCourseOngoing: PropTypes.func.isRequired,
     getCourseOngoings: PropTypes.func.isRequired,
-    allCourseOngoings: PropTypes.array.isRequired
+    allCourseOngoings: PropTypes.array.isRequired,
+    getExtendedCoursessById: PropTypes.func.isRequired,
+    coursesExtended: PropTypes.array,
+    fetchingExtendedCourses: PropTypes.bool.isRequired
 }
 
 const mapStateToProps = (state) => ({
     error: state.error,
-    allCourseOngoings: state.courseOngoing.allCourseOngoings
+    allCourseOngoings: state.courseOngoing.allCourseOngoings,
+    coursesExtended: state.course.extendedCoursesByIds,
+    fetchingExtendedCourses: state.course.fetchingExtendedCourses
 });
 
 export default connect(mapStateToProps, {
     deleteCourseOngoing,
-    getCourseOngoings
+    getCourseOngoings,
+    getExtendedCoursessById
 })(ListAllCourseOngoings);
