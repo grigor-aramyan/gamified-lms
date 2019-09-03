30d4cc0adf5655872c672306fb36f0c110ce7898
grigor-aramyan <grigori.aramyan@gmail.com>
2019-09-03 22:12:10 +0400

    add initial styles.css with responsive media queries

--- src/server/server.js
+++ src/server/server.js
@@ -58,6 +58,7 @@ server.get('/*', (req, res) => {
                     href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
                     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
                     crossorigin="anonymous">
+                <link rel="stylesheet" href="/css/styles.css">
             </head>
             <body>
                 <div id="root">${initialMarkup}</div>
