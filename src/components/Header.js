a44e2267fcc5130aef5df407447cbe8d03e15149
grigor-aramyan <grigori.aramyan@gmail.com>
2019-09-03 23:55:28 +0400

    add initial structure and styles of left-side main menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-    Navbar,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink
-} from 'reactstrap';
 import PropTypes from 'prop-types';
+
+import {
+    Container,
+    Navbar,
+    NavbarBrand
+} from 'reactstrap';
 
 import { logoutInit } from '../actions/authActions';
 
 class Header extends Component {
-    
-    onSignOut = () => {
-        this.props.logoutInit();
+    toggle = () => {
+        const mainMenu = document.getElementById('menu');
+        mainMenu.classList.toggle('menu-opened');
     }
 
     render() {
@@ -25,53 +24,19 @@ class Header extends Component {
 
         return(
             <div>
-                <Navbar color='dark' dark expand='md'>
+                <Navbar color='dark' dark>
                     <NavbarBrand href='/'>Gamified LMS</NavbarBrand>
-                    <Nav navbar className='ml-auto'>
-                        { !isAuthenticated ? 
-                            <NavItem>
-                                <NavLink href='/'>Login</NavLink>
-                            </NavItem> :
-                            null
-                        }
-                        { !isAuthenticated ? 
-                            <NavItem>
-                                <NavLink href='/register'>Register</NavLink>
-                            </NavItem> :
-                            null
-                        }
-                        { isAuthenticated ? 
-                            <NavItem>
-                                <NavLink href='/lessons'>My lessons</NavLink>
-                            </NavItem> :
-                            null
-                        }
-                        { isAuthenticated ? 
-                            <NavItem>
-                                <NavLink href='/courses'>My courses</NavLink>
-                            </NavItem> :
-                            null
-                        }
-                        { (isAuthenticated && !isTeacher) ? 
-                            <NavItem>
-                                <NavLink href='/lesson_ongoings'>Lesson Ongoings</NavLink>
-                            </NavItem> :
-                            null
-                        }
-                        { (isAuthenticated && !isTeacher) ?
-                            <NavItem>
-                                <NavLink href='/course_ongoings'>Course Ongoings</NavLink>
-                            </NavItem> :
-                            null
-                        }
-                        { isAuthenticated ?
-                            <NavItem>
-                                <NavLink href='/' onClick={this.onSignOut}>Sign Out</NavLink>
-                            </NavItem> :
-                            null
-                        }
-                    </Nav>
+                    <a onClick={this.toggle} href='#'>
+                        <img src='/images/hamburger_menu.png'
+                            style={{
+                                width: '30px',
+                                height: '30px'
+                            }} />
+                    </a>
                 </Navbar>
+                <div id='menu' className='main-menu'>
+                    
+                </div>
             </div>
         );
     }
@@ -86,7 +51,7 @@ Header.propTypes = {
 const mapStateToProps = (state) => ({
     isAuthenticated: state.auth.isAuthenticated,
     isTeacher: state.auth.isTeacher
-})
+});
 
 export default connect(mapStateToProps, {
     logoutInit
---
+++ src/components/Header2.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+import PropTypes from 'prop-types';
+
+import { logoutInit } from '../actions/authActions';
+
+class Header2 extends Component {
+    
+    onSignOut = () => {
+        this.props.logoutInit();
+    }
+
+    render() {
+        const {
+            isAuthenticated,
+            isTeacher
+        } = this.props;
+
+        return(
+            <div>
+                <Navbar color='dark' dark expand='md'>
+                    <NavbarBrand href='/'>Gamified LMS</NavbarBrand>
+                    <Nav navbar className='ml-auto'>
+                        { !isAuthenticated ? 
+                            <NavItem>
+                                <NavLink href='/'>Login</NavLink>
+                            </NavItem> :
+                            null
+                        }
+                        { !isAuthenticated ? 
+                            <NavItem>
+                                <NavLink href='/register'>Register</NavLink>
+                            </NavItem> :
+                            null
+                        }
+                        { isAuthenticated ? 
+                            <NavItem>
+                                <NavLink href='/lessons'>My lessons</NavLink>
+                            </NavItem> :
+                            null
+                        }
+                        { isAuthenticated ? 
+                            <NavItem>
+                                <NavLink href='/courses'>My courses</NavLink>
+                            </NavItem> :
+                            null
+                        }
+                        { (isAuthenticated && !isTeacher) ? 
+                            <NavItem>
+                                <NavLink href='/lesson_ongoings'>Lesson Ongoings</NavLink>
+                            </NavItem> :
+                            null
+                        }
+                        { (isAuthenticated && !isTeacher) ?
+                            <NavItem>
+                                <NavLink href='/course_ongoings'>Course Ongoings</NavLink>
+                            </NavItem> :
+                            null
+                        }
+                        { isAuthenticated ?
+                            <NavItem>
+                                <NavLink href='/' onClick={this.onSignOut}>Sign Out</NavLink>
+                            </NavItem> :
+                            null
+                        }
+                    </Nav>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+Header2.propTypes = {
+    isAuthenticated: PropTypes.bool,
+    isTeacher: PropTypes.bool.isRequired,
+    logoutInit: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    isTeacher: state.auth.isTeacher
+})
+
+export default connect(mapStateToProps, {
+    logoutInit
+})(Header2);
