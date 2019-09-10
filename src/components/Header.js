4bdffaf46e2529012b1b3d369ccf21a3ccd4669a
grigor-aramyan <grigori.aramyan@gmail.com>
2019-09-10 23:11:57 +0400

    add platform production name to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,7 +47,7 @@ class Header extends Component {
         return(
             <div>
                 <Navbar color='dark' dark>
-                    <NavbarBrand href='/'>Gamified LMS</NavbarBrand>
+                    <NavbarBrand href='/'>Nidha Learning</NavbarBrand>
                     <a onClick={this.toggle} href='#'>
                         <img src='/images/hamburger_menu.png'
                             style={{
