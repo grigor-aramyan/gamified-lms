1fe712d6f07c0d1edc9e603afddbc430c57fc2d5
grigor-aramyan <grigori.aramyan@gmail.com>
2019-09-27 18:44:53 +0400

    add update lesson sat base redux type and action

--- src/actions/exerciseActions.js
+++ src/actions/exerciseActions.js
@@ -4,12 +4,33 @@ import { returnErrors } from './errorActions';
 import {
     CREATE_MANY_SINGLE_ANSWER_TEST_EXERCISES,
     GET_SINGLE_ANSWER_TEST_EXERCISES_BY_LESSON_ID,
-    GET_SAT_EXERCISES_INITIATE
+    GET_SAT_EXERCISES_INITIATE,
+    LESSON_FOR_TEACHER_UPDATE_SAT_BASE
 } from './types';
 import { tokenConfig, baseUri } from './authActions';
 
 // Constants
 const API_URI = baseUri + '/api/exercises';
+
+export const updateLessonSatBase = (deletedIds, exercises) => (dispatch, getState) => {
+    const uri = `${API_URI}/singleAnswerTestQuestion/updateLessonSatBase`;
+
+    const body = {
+        deletedIds,
+        exercises
+    };
+
+    axios.post(uri, body, tokenConfig(getState))
+        .then(res => {
+            dispatch({
+                type: LESSON_FOR_TEACHER_UPDATE_SAT_BASE,
+                payload: res.data.exercises
+            });
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status));
+        });
+}
 
 export const createManySATExercises = (exercisesData) => (dispatch, getState) => {
     const uri = `${API_URI}/singleAnswerTestQuestion`;
